01ba3c06e54d6636dfcd65a4280b3bdb637fcb52
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-23 14:39:50 +0100

    added milestone 3

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ const app = createApp({
     name: 'Vue Slider',
     data: () => ({
         toDoList,
+        taskContent: ''
     }),
     methods:{
         removeTask(id) {
             this.toDoList = this.toDoList.filter(list => id !== list.id);
+        },
+        addTask() {
+            const newTask = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.taskContent
+            }
+            this.toDoList.push(newTask);
         }
     }
 })
